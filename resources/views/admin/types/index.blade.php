7ff6f15cff0f89020373c12ffaaa802c17f020e7
MatteoSanson <matteo.sanson@gmail.com>
2024-02-13 21:36:57 +0100

    fix: messages

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -13,7 +13,7 @@
                 role="alert" aria-live="assertive" aria-atomic="true">
                 <div class="toast-header">
                     <strong
-                        class="me-auto">{{ session('notification_type') === 'danger' ? 'Error' : 'Notification' }}</strong>
+                        class="me-auto">{{ session('notification_type') === 'danger' ? 'Delete' : 'Notification' }}</strong>
                     <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
                 </div>
                 <div class="toast-body">
