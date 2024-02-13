25541a03c4edb0bc6b38b42ec2114a489f068201
MatteoSanson <matteo.sanson@gmail.com>
2024-02-13 01:57:41 +0100

    feat: destroy element

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -126,6 +126,8 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
+        $project->technologies()->sync([]);
+
         $project->delete();
         return redirect()->route('admin.projects.index')->with('message', 'Hai eliminato un progetto!');
     }
